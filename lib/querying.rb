a903bf3a985a6cac05d64456c0efbf28d7736e80
kodyclemens <kody.clemens@gmail.com>
2018-12-05 21:04:16 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,14 +1,53 @@
+# CREATE TABLE series (
+#   id INTEGER PRIMARY KEY,
+#   title TEXT,
+#   author_id INTEGER,
+#   subgenre_id INTEGER
+# );
+
+# CREATE TABLE subgenres (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT
+# );
+
+# CREATE TABLE authors (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT
+# );
+
+# CREATE TABLE books (
+#   id INTEGER PRIMARY KEY,
+#   title TEXT,
+#   year INTEGER,
+#   series_id INTEGER
+# );
+
+# CREATE TABLE characters (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   species TEXT,
+#   motto TEXT,
+#   series_id INTEGER,
+#   author_id INTEGER
+# );
+
+# CREATE TABLE character_books (
+#   id INTEGER PRIMARY KEY,
+#   book_id INTEGER,
+#   character_id INTEGER
+# );
+
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books JOIN series ON books.series_id = series.id WHERE series.id = 1 ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY length(characters.motto) DESC LIMIT 1"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) FROM characters WHERE characters.species = 'human'"
 end
 
 def select_name_and_series_subgenres_of_authors
