3775a95a2142496d3919b3d7bac322231071516f
kodyclemens <kody.clemens@gmail.com>
2018-12-05 21:11:12 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,11 +47,11 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) FROM characters WHERE characters.species = 'human'"
+  "SELECT characters.species, COUNT(*) FROM characters GROUP BY species ORDER BY COUNT(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors JOIN series ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
