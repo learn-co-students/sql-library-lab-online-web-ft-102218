13d073900f4faa662cb120865bc537ce28a56b44
kodyclemens <kody.clemens@gmail.com>
2018-12-06 08:43:35 -0500

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,44 +1,9 @@
-# CREATE TABLE series (
-#   id INTEGER PRIMARY KEY,
-#   title TEXT,
-#   author_id INTEGER,
-#   subgenre_id INTEGER
-# );
-
-# CREATE TABLE subgenres (
-#   id INTEGER PRIMARY KEY,
-#   name TEXT
-# );
-
-# CREATE TABLE authors (
-#   id INTEGER PRIMARY KEY,
-#   name TEXT
-# );
-
-# CREATE TABLE books (
-#   id INTEGER PRIMARY KEY,
-#   title TEXT,
-#   year INTEGER,
-#   series_id INTEGER
-# );
-
-# CREATE TABLE characters (
-#   id INTEGER PRIMARY KEY,
-#   name TEXT,
-#   species TEXT,
-#   motto TEXT,
-#   series_id INTEGER,
-#   author_id INTEGER
-# );
-
-# CREATE TABLE character_books (
-#   id INTEGER PRIMARY KEY,
-#   book_id INTEGER,
-#   character_id INTEGER
-# );
 
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT books.title, books.year FROM books JOIN series ON books.series_id = series.id WHERE series.id = 1 ORDER BY books.year"
+  "SELECT books.title, books.year
+  FROM books
+  JOIN series ON books.series_id = series.id
+  WHERE series.id = 1 ORDER BY books.year"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -51,13 +16,72 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors JOIN series ON series.author_id = authors.id JOIN subgenres ON series.subgenre_id = subgenres.id"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  JOIN series ON authors.id = series.author_id
+  JOIN subgenres ON authors.id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  JOIN books
+  ON series.id = books.series_id
+  JOIN character_books
+  ON character_books.book_id = books.id
+  JOIN characters
+  ON characters.id = character_books.character_id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(*) DESC
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*)
+  FROM characters
+  JOIN character_books
+  ON characters.id = character_books.character_id
+  JOIN books
+  ON books.id = character_books.book_id
+  GROUP BY characters.name
+  ORDER BY COUNT(*) DESC"
 end
+      # CREATE TABLE series (
+      #   id INTEGER PRIMARY KEY,
+      #   title TEXT,
+      #   author_id INTEGER,
+      #   subgenre_id INTEGER
+      # );
+      
+      # CREATE TABLE subgenres (
+      #   id INTEGER PRIMARY KEY,
+      #   name TEXT
+      # );
+      
+      # CREATE TABLE authors (
+      #   id INTEGER PRIMARY KEY,
+      #   name TEXT
+      # );
+      
+      # CREATE TABLE books (
+      #   id INTEGER PRIMARY KEY,
+      #   title TEXT,
+      #   year INTEGER,
+      #   series_id INTEGER
+      # );
+      
+      # CREATE TABLE characters (
+      #   id INTEGER PRIMARY KEY,
+      #   name TEXT,
+      #   species TEXT,
+      #   motto TEXT,
+      #   series_id INTEGER,
+      #   author_id INTEGER
+      # );
+      
+      # CREATE TABLE character_books (
+      #   id INTEGER PRIMARY KEY,
+      #   book_id INTEGER,
+      #   character_id INTEGER
+      # );
